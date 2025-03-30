953df3835581aff354ea96ea5f963b9f76ebfa0f
huanlv620 <huanlv620@gmail.com>
2025-03-30 13:37:49 +0700

    Add header section

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,1 +1,17 @@
-console.log("Run app!");
+const modal = document.querySelector("#modal");
+const iconSearch = document.querySelector(".header__icon-search");
+const modalIconArrow = document.querySelector(".modal__icon-arrow");
+
+iconSearch.onclick = (e) => {
+  modal.classList.add("show");
+};
+
+modalIconArrow.onclick = (e) => {
+  modal.classList.remove("show");
+};
+
+modal.onclick = (e) => {
+  if (modal === e.target) {
+    modal.classList.remove("show");
+  }
+};
