14dc6b3154795a23f09e52fcd018fe5d2e6068bd
huanlv620 <huanlv620@gmail.com>
2025-04-01 16:50:59 +0700

    ui viegazine page #1 of 2

--- js/style.js
+++ js/style.js
@@ -2,12 +2,22 @@ const modal = document.querySelector("#modal");
 const iconSearch = document.querySelector(".header__icon-search");
 const modalIconArrow = document.querySelector(".modal__icon-arrow");
 
+const audio = document.getElementById("background-music");
+const playPauseButton = document.getElementById("play-pause-button");
+const playIcon = document.getElementById("play-music");
+const pauseIcon = document.getElementById("pause-music");
+let isPlaying = false;
+
 const header = document.querySelector(".header");
 const logo = document.querySelector(".logo");
 const logoSvg = document.querySelector(".logo__svg");
 
 const buttonDecorMessage = document.querySelector(".decor-message");
 const buttonDecorFeedback = document.querySelector(".decor-feedback");
+
+// show list subject
+const btnMore = document.querySelector(".fascinating-books__more-btn");
+const listSubject = document.querySelector(".fascinating-books__more-list");
 
 iconSearch.onclick = () => {
   modal.classList.add("show");
@@ -23,6 +33,55 @@ modal.onclick = (e) => {
   }
 };
 
+// Slider swiper
+function findMissingNumber(arr) {
+  let n = arr.length + 1;
+  let sum = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  let sumMax = (n * (n + 1)) / 2;
+  return sumMax - sum;
+}
+
+function togglePlayPause() {
+  if (isPlaying) {
+    audio.pause();
+    playIcon.classList.remove("hidden");
+    pauseIcon.classList.add("hidden");
+  } else {
+    audio.play();
+    playIcon.classList.add("hidden");
+    pauseIcon.classList.remove("hidden");
+  }
+  isPlaying = !isPlaying;
+}
+
+// playPauseButton.addEventListener("click", togglePlayPause);
+
+var swiper = new Swiper(".swiper", {
+  grabCursor: true,
+  initialSlide: 4,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  spaceBetween: 10,
+  speed: 1000,
+  freeMode: false,
+  mousewheel: {
+    thresholdDelta: 30,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  on: {
+    click(event) {
+      swiper.slideTo(this.clickedIndex);
+    },
+  },
+});
+
 // Gộp sự kiện scroll
 window.addEventListener("scroll", () => {
   const scrollPosition = window.scrollY;
@@ -32,7 +91,7 @@ window.addEventListener("scroll", () => {
   // Cập nhật header khi cuộn
   if (scrollPosition > 300) {
     header.style.color = "#8917ee";
-    header.style.background = "#fafafa";
+    header.style.background = "#fff";
     logoSvg.classList.add("logo__small");
     buttonDecorMessage.classList.add("show");
   } else {
@@ -48,32 +107,6 @@ window.addEventListener("scroll", () => {
   }
 });
 
-// Swiper
-var swiper = new Swiper(".swiper", {
-  effect: "coverflow",
-  grabCursor: true,
-  centeredSlides: true,
-  initialSlide: 2,
-  speed: 600,
-  preventClicks: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 0,
-    stretch: 80,
-    depth: 350,
-    modifier: 1,
-    slideShadows: true,
-  },
-  on: {
-    click(event) {
-      swiper.slideTo(this.clickedIndex);
-    },
-  },
-});
-
-// Thêm sự kiện hover để chuyển slide
-document.querySelectorAll(".swiper-slide").forEach((slide, index) => {
-  slide.addEventListener("mouseenter", () => {
-    swiper.slideTo(index);
-  });
-});
+btnMore.onclick = () => {
+  listSubject.classList.toggle("show");
+};
