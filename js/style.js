369a45fd771b972a50ae8136ea0f2b81166de600
huanlv620 <huanlv620@gmail.com>
2025-04-02 12:10:38 +0700

    loading bar for detail page

--- js/style.js
+++ js/style.js
@@ -9,28 +9,13 @@ const logoSvg = document.querySelector(".logo__svg");
 const buttonDecorMessage = document.querySelector(".decor-message");
 const buttonDecorFeedback = document.querySelector(".decor-feedback");
 
-// modal detail
+// modal detail// modal detail
 const btnDetailShowModal = document.querySelector(".reading-list-btn");
 const modalDetail = document.querySelector(".modal-detail");
 const btnDecorItemOne = document.querySelector(".reading-list-btn__item-one");
 const btnDecorItemThree = document.querySelector(
   ".reading-list-btn__item-three"
 );
-
-console.log(btnDecorItemOne);
-console.log(btnDecorItemThree);
-
-btnDetailShowModal.onclick = () => {
-  modalDetail.classList.toggle("show");
-  btnDecorItemOne.classList.toggle("active");
-  btnDecorItemThree.classList.toggle("active");
-};
-
-modalDetail.onclick = () => {
-  modalDetail.classList.remove("show");
-  btnDecorItemOne.classList.toggle("active");
-  btnDecorItemThree.classList.toggle("active");
-};
 
 // Modal header
 iconSearch.onclick = () => {
@@ -102,4 +87,23 @@ document.querySelectorAll(".swiper-slide").forEach((slide, index) => {
   });
 });
 
-// modal detail
+// modal detail// modal detail
+btnDetailShowModal.onclick = () => {
+  modalDetail.classList.toggle("show");
+  btnDecorItemOne.classList.toggle("active");
+  btnDecorItemThree.classList.toggle("active");
+};
+
+modalDetail.onclick = () => {
+  modalDetail.classList.remove("show");
+  btnDecorItemOne.classList.toggle("active");
+  btnDecorItemThree.classList.toggle("active");
+};
+
+window.addEventListener("scroll", function () {
+  let scrollTop = window.scrollY; // Số pixel đã cuộn từ trên xuống
+  let scrollHeight = document.documentElement.scrollHeight - window.innerHeight; // Tổng chiều cao có thể cuộn
+  let progress = (scrollTop / scrollHeight) * 100; // Tính phần trăm
+
+  document.querySelector(".progress-bar").style.width = progress + "%";
+});
